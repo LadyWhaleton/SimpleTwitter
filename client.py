627e5f778e1069222628929c56b58f1fcb9aa891
StephanieTong <stong002@ucr.edu>
2015-05-22 20:13:51 -0700

    Client connection successful.

--- client.py
+++ client.py
@@ -1,24 +1,13 @@
-from check import ip_checksum
 import socket
-import select
 import sys
 import time
 
-# this is the code for the client
-# in rdt3.0, the client is the sender in this case
-def isSameChecksum (old, new):
-	if old != new:
-		print "Error: Invalid checksum!"
 
-def make_pkt(seqNum, msg, checksum):
-	packet = str(seqNum) + checksum + msg[seqNum]
-	return packet
 
-def getacknum(packet):
-	acknum = packet[0:1]
 
-# -----------------------------------------------------------
-# this is main
+# ========================== M A I N ===========================
+host = ''
+port = 2124
 
 try:
 	sock = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
@@ -27,47 +16,16 @@ except socket.error, msg:
 	print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message: ' + msg[1] 
 	sys.exit();
 
-host = ''
-port = 2124
+# try to connect to the server
+ret = sock.connect_ex ( (host, port))
 
-
-base = 0
-nextseqnum = 0
-timeOut = False
+if ret > 0:
+	print 'Error: Unable to connect to server!'
+	exit(0)
 
 while (1):
-	
-	if not(timeOut):
-		checksum = ip_checksum (msg)
-		packet = make_pkt (nextseqnum, msg, checksum)
-		sock.sendto (packet, (host, port))
-		
-	# select
-	inputs = [sock]
-	outputs = []
-	timeout = 5
-	readable, writable, exceptional = select.select(inputs, outputs, inputs, timeout)
-	
-	data = []
-	serverAddr = []	
-
-	for tempSocket in readable:
-		data, serverAddr = tempSocket.recvfrom(2048)
-		print 'Message[' + serverAddr[0] + ':' + str(serverAddr[1]) + '] ' + data.strip()
-		isSameChecksum (data[1:3],checksum)
-		timeOut = False	
-	
-	# handle timeouts here (slide 48 from lecture 5-6-7)	
-	if not data:
-		print "Error: Timed out! Resending packet!"
-		packet = make_pkt(nextseqnum, msg, checksum)
-		sock.sendto (packet, (host, port))
-		timeOut = True
-		
-	
-	if not(timeOut):	
-		nextseqnum += 1
-		
+	msg = "hi"	
+				
 
 # now we close the socket
 sock.close()
